b584c8401020279fb9b43ba8761d825991b32678
ozan <ozanakaydin@gmail.com>
2024-04-26 20:50:46 +0300

    Mvc - New Controllers and Actions created

---
+++ src/KanbanBoard.WebMvc/Controllers/IssueController.cs
@@ -0,0 +1,23 @@
+﻿using KanbanBoard.Application.Dtos.IssueDtos;
+using KanbanBoard.Application.Services.Manager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KanbanBoard.WebMvc.Controllers;
+
+public class IssueController : Controller
+{
+
+    private readonly IServiceManager _manager;
+    public IssueController(IServiceManager manager)
+    {
+        _manager = manager;
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> UpdateIssue(UpdateIssueStatusDto updateIssue)
+    {
+        var boardId = await _manager.Issue.UpdateIssueStatusAndOrderAsync(updateIssue);
+        return RedirectToAction("Index", "Status", new { id= boardId });
+    }
+}
---
+++ src/KanbanBoard.WebMvc/Controllers/StatusController.cs
@@ -0,0 +1,23 @@
+﻿using KanbanBoard.Application.Services.Manager;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KanbanBoard.WebMvc.Controllers;
+
+public class StatusController : Controller
+{
+    private readonly IServiceManager _manager;
+
+    public StatusController(IServiceManager manager)
+    {
+        _manager = manager;
+    }
+
+    public async Task<IActionResult> Index(string id)
+    {
+        var board = await _manager.Board.GetBoardByIdAsync(id);
+        var status = await _manager.Status.GetStatusByBoardIdAsync(id);
+        ViewData["boardTitle"] = board.Title;
+        return View(status);
+    }
+
+}
