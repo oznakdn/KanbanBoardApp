d7c2fcddfa6ed8c9d8aed54744fcc6729ab249e2
ozan <ozanakaydin@gmail.com>
2024-04-28 16:48:52 +0300

    WebMvc - StatusController/Create action created

--- src/KanbanBoard.WebMvc/Controllers/StatusController.cs
+++ src/KanbanBoard.WebMvc/Controllers/StatusController.cs
@@ -1,4 +1,5 @@
-﻿using KanbanBoard.Application.Services.Manager;
+﻿using KanbanBoard.Application.Dtos.StatusDtos;
+using KanbanBoard.Application.Services.Manager;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KanbanBoard.WebMvc.Controllers;
@@ -16,8 +17,15 @@ public class StatusController : Controller
     {
         var board = await _manager.Board.GetBoardByIdAsync(id);
         var status = await _manager.Status.GetStatusByBoardIdAsync(id);
+        ViewData["boardId"] = id;
         ViewData["boardTitle"] = board.Title;
         return View(status);
+    }
+
+    public async Task<IActionResult>Create(CreateStatusDto createStatus)
+    {
+        await _manager.Status.CreateStatusAsync(createStatus);
+        return Json(new { redirectToUrl = Url.Action("Index", "Status", new { id = createStatus.BoardId }) });
     }
 
 }
