c49c4318d5529a93f309b9368e74fe5df2f75aa5
ozan <ozanakaydin@gmail.com>
2024-05-02 19:25:16 +0300

    WebMvc - GetIssueStatistics view component created

---
+++ src/KanbanBoard.WebMvc/ViewComponents/GetIssueStatisticsViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using KanbanBoard.Application.Services.Manager;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace KanbanBoard.WebMvc.ViewComponents;
+
+public class GetIssueStatisticsViewComponent : ViewComponent
+{
+    private readonly IServiceManager _serviceManager;
+
+    public GetIssueStatisticsViewComponent(IServiceManager serviceManager)
+    {
+        _serviceManager = serviceManager;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var result = await _serviceManager.Issue.GetIssueStatisticsAsync();
+        return View(result);
+    }
+}
