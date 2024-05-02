738bb706ae5ada96e5d408430b345343ca83819c
oznakdn <ozanakaydin@gmail.com>
2024-05-02 11:02:21 +0300

    Core - added the ProfilePicture field to User model

--- src/KanbanBoard.Core/Abstracts/AbstractModel.cs
+++ src/KanbanBoard.Core/Abstracts/AbstractModel.cs
@@ -5,5 +5,5 @@ namespace KanbanBoard.Core.Abstracts;
 public abstract class AbstractModel  : IModel
 {
     public string Id { get ; set ; } = Guid.NewGuid().ToString();
-    public DateTime CreatedDate { get; set; } = DateTime.Now;
+    public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
 }
--- src/KanbanBoard.Core/Models/User.cs
+++ src/KanbanBoard.Core/Models/User.cs
@@ -6,5 +6,5 @@ public class User : IdentityUser
 {
     public ICollection<UserIssues>UserIssues{ get; set; } = new HashSet<UserIssues>();
     public ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
-
+    public string? ProfilePicture { get; set; }
 }
