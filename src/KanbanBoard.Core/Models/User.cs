b4c98b8f3eecc999cfe17a9d906214cc29c895ec
oznakdn <ozanakaydin@gmail.com>
2024-05-03 11:26:07 +0300

    Core - User model updated and UserTitle model created

--- src/KanbanBoard.Core/Models/User.cs
+++ src/KanbanBoard.Core/Models/User.cs
@@ -7,4 +7,10 @@ public class User : IdentityUser
     public ICollection<UserIssues>UserIssues{ get; set; } = new HashSet<UserIssues>();
     public ICollection<Comment> Comments { get; set; } = new HashSet<Comment>();
     public string? ProfilePicture { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string TitleId { get; set; }
+    public UserTitle? Title { get; set; }
+    public string Fullname => $"{FirstName} {LastName}";
+
 }
---
+++ src/KanbanBoard.Core/Models/UserTitle.cs
@@ -0,0 +1,9 @@
+﻿using KanbanBoard.Core.Abstracts;
+
+namespace KanbanBoard.Core.Models;
+
+public class UserTitle : AbstractModel
+{
+    public string Title { get; set; }
+    public ICollection<User> Users { get; set; } = new HashSet<User>();
+}
